abee1494b488496e3c451ff6804882624a3007e1
Can Sahin <csahin@imobilecode.com>
2017-11-01 20:20:51 +0300

    project added with readme

---
+++ Example/Moya-PromiseKit-Service/Examples/CarService/CarDataService.swift
@@ -0,0 +1,9 @@
+//
+//  CarDataService.swift
+//  Moya-PromiseKit-Service
+//
+//  Created by Can Sahin on 01/11/2017.
+//  Copyright © 2017 Can Sahin. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Moya-PromiseKit-Service/Examples/CarService/CarTarget.swift
@@ -0,0 +1,9 @@
+//
+//  CarTarget.swift
+//  Moya-PromiseKit-Service
+//
+//  Created by Can Sahin on 01/11/2017.
+//  Copyright © 2017 Can Sahin. All rights reserved.
+//
+
+import Foundation
