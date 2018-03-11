3e6ffbc199107fcf8e8e3df45669dbdec94dc749
Can Sahin <cann2005@gmail.com>
2018-03-11 03:34:39 +0300

    Pods folder removed

--- src/Pods/Target Support Files/Pods-MoyaPromise/Pods-MoyaPromise-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoyaPromise : NSObject
-@end
-@implementation PodsDummy_Pods_MoyaPromise
-@end
--- src/Pods/Target Support Files/Pods-MoyaPromiseTests/Pods-MoyaPromiseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoyaPromiseTests : NSObject
-@end
-@implementation PodsDummy_Pods_MoyaPromiseTests
-@end
