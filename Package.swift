439797017a30330d4505d3ec41b772878c46a3b1
Yegor Kazantsev <develop74@datum-group.ru>
2021-02-08 12:36:05 +0300

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,17 @@
 // swift-tools-version:5.3
-/*
-  s.name         = "MoyaPromise"
-  s.version      = "1.0.0"
-  s.summary      = "PromiseKit-oriented 'DataService' layer on top of Moya"
+// s.name         = "MoyaPromise"
+// s.version      = "1.0.0"
+// s.summary      = "PromiseKit-oriented 'DataService' layer on top of Moya"
+// s.description  = <<-DESC
+//    A 'DataService' layer implemented with PromiseKit on the top of Moya with extra functionalities using the power of Promis
+// DESC
+//  s.homepage     = "https://github.com/Can-Sahin/MoyaPromise"
+//  s.license = { :type => "MIT", :file => "LICENSE" }
+//  s.author             = { "Can-Sahin" => "cann2005@gmail.com" }
+//  s.swift_version = "4.0"
+//  s.source       = { :git => "https://github.com/Can-Sahin/MoyaPromise.git", :tag => "#{s.version}" }
+//  s.source_files  = 'src/MoyaPromise/*.swift'
 
-  s.description  = <<-DESC
-    A 'DataService' layer implemented with PromiseKit on the top of Moya with extra functionalities using the power of Promis
-    DESC
-
-  s.homepage     = "https://github.com/Can-Sahin/MoyaPromise"
-  s.license = { :type => "MIT", :file => "LICENSE" }
-  s.author             = { "Can-Sahin" => "cann2005@gmail.com" }
-  s.swift_version = "4.0"
-
-  s.source       = { :git => "https://github.com/Can-Sahin/MoyaPromise.git", :tag => "#{s.version}" }
-  s.source_files  = 'src/MoyaPromise/*.swift'
-*/
 
 import PackageDescription
 
@@ -37,7 +33,7 @@ let package = Package(
         .package(url: "https://github.com/Moya/Moya.git", from: "11.0.1")
     ],
     targets: [
-        .target(name: "MoyaPromise", dependencies: []),
+        .target(name: "MoyaPromise"),
         .testTarget(name: "MoyaPromiseTests", dependencies: ["MoyaPromise"])
     ]
 )
