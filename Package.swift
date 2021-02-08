2858abf7ee06292ebaf368efa87ae361d6099bf5
Yegor Kazantsev <develop74@datum-group.ru>
2021-02-08 14:47:08 +0300

    fix depences

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.7.0"),
-        .package(url: "https://github.com/PromiseKit/PMKAlamofire.git", from: "6.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.2.1"),
         .package(url: "https://github.com/Moya/Moya.git", from: "11.0.1")
     ],
